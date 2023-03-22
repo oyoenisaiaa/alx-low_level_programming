afa663343cec28fa03db05ff523c4b85ac64aed7
oyoenisaiaa <oyoenisaiaa@gmail.com>
2023-03-21 22:42:16 -0700

    functions

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,15 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	for (count = 0; count < 50; count++)
+	{
 		sum = fib1 + fib2;
-	printf("%lu", sum);
-	fib1 = fib2;
-	fib2 = sum;
-	if (count == 49)
-		printf("\n");
-	else
-		printf(", ");
-}
-return (0);
+		printf("%lu", sum);
+		fib1 = fib2;
+		fib2 = sum;
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
 }
