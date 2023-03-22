94e2d64c9ef5812b260df347ab4b0e2c29f9058e
oyoenisaiaa <oyoenisaiaa@gmail.com>
2023-03-22 14:55:38 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - tests function for positive or negative function
+* Return: always 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
