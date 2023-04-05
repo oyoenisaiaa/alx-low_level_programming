487513ad5640c8ce24cb926f17de6d554df96ecf
oyoenisaiaa <oyoenisaiaa@gmail.com>
2023-04-05 16:16:11 -0700

    puts-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar (*s);
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_puts_recursion(s + 1);
-	}
+	_putchar (*s);
+	_puts_recursion(s + 1);
 }
