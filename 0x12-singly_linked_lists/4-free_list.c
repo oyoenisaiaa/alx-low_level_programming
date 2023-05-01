6bec6d958b9092b86be3f6f185f313c9b1b0e46a
Oyoenisai Andrew-Essien <125687320+oyoenisaiaa@users.noreply.github.com>
2023-05-01 06:13:40 -0400

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,12 @@ void free_list(list_t *head)
 {
 	list_t *current, *next;
 
-	current = head
-	while (head != NULL)
-	{
-		next = current->next;
-		free(current->str);
-		free(current);
-		current = next;
+        current = head;
+        while (current != NULL)
+        {
+                next = current->next;
+                free(current->str);
+                free(current);
+                current = next;
 	}
 }
